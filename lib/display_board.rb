d11ae6fdae329ff162ba6fca8bf7d4e9265163e7
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-17 19:59:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,13 @@
 # out the current state.
 
 def display_board(board)
-  
+  #row 1 
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  
+  #row2
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  #row3
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "-----------"
 end 
